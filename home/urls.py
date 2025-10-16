4429480615a91e896d4a2288c037705dbb2c496c
Ujaan Rakshit <ujaanrakshit@gmail.com>
2025-10-15 23:38:58 -0400

    Implemented local map page

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home.index'),
     path('about', views.about, name='home.about'),
+    path('local-popularity-map', views.local_popularity_map, name='home.local_popularity_map'),
 ]
--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-27@72v%qbv!5p&q18jxno%7%=ljoz-m=tfb@3hmqruvj9)0i1t'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-27@72v%qbv!5p&q18jxno%7%=ljoz-m=tfb@3hmqruvj9)0i1t')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -68,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'moviesstore.context_processors.google_maps_api_key',
             ],
         },
     },
@@ -134,3 +136,6 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Google Maps API Key
+GOOGLE_MAPS_API_KEY = config('GOOGLE_MAPS_API_KEY', default='')
